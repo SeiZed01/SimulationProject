5b23af1b6259863b1cbbec6e9cad42a6feb11c04
Gaebuidhe <faolannus@gmail.com>
2017-04-10 18:09:25 -0400

    Update World.C

--- World.C
+++ World.C
@@ -1,1 +1,27 @@
 // Initialize Functions of World Header
+#include<iostream>
+#include "World.h"
+
+using namespace std;
+
+World::World(){
+	grass = true;
+	day = 0;
+}
+
+World::~World(){
+}
+
+void World::setGrasstrue(){
+	grass = true;
+}
+
+void World::eatGrass(){
+	grass = false;
+	growth = 9;
+}
+
+void World::grow(){
+	if(grass == false && growth > 0) growth--;
+	else if(grass == false && growth <= 0) setGrasstrue();
+}
