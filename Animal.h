ddf78e8f9bab50fb6aad81f8b89cca36aa83978f
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-11 00:04:25 -0400

    Update Animal.h
    Added getHunger, setHunger func.

--- Animal.h
+++ Animal.h
@@ -11,8 +11,11 @@ class Animal : public World{
     public:
         Animal(bool k): hunger(k){}
         Animal(int day, priority_queue<Item*, vector<Item*>, compareItem>* q1): World(day), q(q1){}
+        void setHunger(bool k){hunger = k;}
+        bool getHunger(){return hunger;}
         void Run(); // Implement death and hunger and etc in Run Func.
         virtual void* Eat(); // Find something to eat within certain parameter then delete pointer. 
         ~Animal();
 };
+
 #endif
