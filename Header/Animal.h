56bb2d7bb1a1d017adbcaf7590a938ff2e5b8568
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-12 20:39:43 -0400

    Update Animal.h

--- Header/Animal.h
+++ Header/Animal.h
@@ -15,7 +15,7 @@ class Animal : public World{
         bool getHunger(){return hunger;}
         void Run(); // Implement death and hunger and etc in Run Func.
         virtual void* Eat(); // Find something to eat within certain parameter then delete pointer. 
-        void doublePopulation();
+        virtual void doublePopulation();
         ~Animal();
 };
 
