52016bfd0b32c28ffd1ad6ba11209d9b3c68b883
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-12 20:06:58 -0400

    Update Grassland.h

--- Header/Grassland.h
+++ Header/Grassland.h
@@ -15,7 +15,7 @@ class Grassland : public World{
     public:
         Grassland(int day, priority_queue<World*, vector<World*>, compareItem>* q1): World(day), q(q1){}
         Rabbit* getRabbitClose(int x, int y);
-        void grow(); // Return true if grow
+        void grow(); 
         void eatGrass(int x, int y, Rabbit*);
         void Run();
         ~Grassland();
