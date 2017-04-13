9dce63cb4259c01ef22299b93df1ed7c31f140a7
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-12 23:14:41 -0400

    Update Grassland.h

--- Header/Grassland.h
+++ Header/Grassland.h
@@ -10,12 +10,14 @@ using namespace std;
 class Grassland : public World{
     Cells** cell;
     priority_queue<World*, vector<World*>, compareItem>* q;
-    int growth; // Gunna use to keep track of 10 days
-    bool grass; 
+    
+    //int growth; Might not need these since theres a lastEaten data in Cell.h.
+    //bool grass; 
+    
     public:
         Grassland(int day, priority_queue<World*, vector<World*>, compareItem>* q1): World(day), q(q1){}
         Rabbit* getRabbitClose(int x, int y);
-        void grow(); 
+        //void grow(); 
         void eatGrass(int x, int y, Rabbit*);
         void Run();
         ~Grassland();
