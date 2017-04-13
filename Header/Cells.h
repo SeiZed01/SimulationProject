4b9c9e4b5d2696489e3ed570cc7ad7a8a4c65e41
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-13 00:22:35 -0400

    Update Cells.h

--- Header/Cells.h
+++ Header/Cells.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Cells : public World{
 public:
-    Animal* a;
+    Animal* a; // Pointing to nothing, just make it point to animal when animal is put within cell.
     int lastEaten; // Use to keep track of the days for grass growth? Maybe set to 10 and when it gets eaten change to 0 then increase 
     // Until 10 again. Only able to eat if lastEaten is 10.
     int x, y;
