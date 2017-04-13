5a0d7c22fdd49e0ac2e9addb25fd4044da798f06
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-12 20:00:52 -0400

    Update Grassland.h

--- Header/Grassland.h
+++ Header/Grassland.h
@@ -2,19 +2,21 @@
 #define GRASSLAND_H
 
 #include "World.h"
+#include "Cells.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Grassland : public World{
+    Cells** cell;
     priority_queue<World*, vector<World*>, compareItem>* q;
-    int growth;
-    bool grass;
+    int growth; // Gunna use to keep track of 10 days
+    bool grass; 
     public:
-        void setGrassTrue();
-        bool grow(); // Return true if grow
-        void eatGrass();
         Grassland(int day, priority_queue<World*, vector<World*>, compareItem>* q1): World(day), q(q1){}
+        Rabbit* getRabbitClose(int x, int y);
+        bool grow(); // Return true if grow
+        void eatGrass(int x, int y, Rabbit*);
         void Run();
         ~Grassland();
 };
