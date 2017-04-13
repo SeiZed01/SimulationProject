b513d871a85384962ee4a24a486bd76fc7e37ea4
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-12 23:05:40 -0400

    Update Cells.h

--- Header/Cells.h
+++ Header/Cells.h
@@ -11,7 +11,7 @@ public:
     int lastEaten; // Use to keep track of the days for grass growth? Maybe set to 10 and when it gets eaten change to 0 then increase 
     // Until 10 again. Only able to eat if lastEaten is 10.
     int x, y;
-    Cells(){a = NULL; lastEaten = 0;}
+    Cells(){a = NULL; lastEaten = 10;}
     ~Cell();
 };
 #endif
