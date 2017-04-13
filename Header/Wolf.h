5e2d050b67b9a9b4d8889d77f95efa087681378b
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-12 20:50:34 -0400

    Update Wolf.h

--- Header/Wolf.h
+++ Header/Wolf.h
@@ -9,6 +9,7 @@ class Wolf : public Animal{
     priority_queue<World*, vector<World*>, compareItem >* q;
     public:
         Wolf(int k, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1): World(k), Animal(hunger), q(q1){}
+        void doublePopulation();
         void* Eat(); // Might change Eat to Roam for all
         void Run(); // Needs work
 };
