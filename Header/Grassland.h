5edfab3838ba13c86c1e26e48e4386e24b07cc6a
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-14 22:04:30 -0400

    Update Grassland.h
    AddAnimal Functions.

--- Header/Grassland.h
+++ Header/Grassland.h
@@ -2,24 +2,30 @@
 #define GRASSLAND_H
 
 #include "World.h"
+#include "Animal.h"
+#include "Wolf.h"
 #include "Cells.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Grassland : public World{
-    Cells** cell;
-    priority_queue<World*, vector<World*>, compareItem>* q;
-    
-    //int growth; Might not need these since theres a lastEaten data in Cell.h.
-    //bool grass; 
-    
+    priority_queue<World*, vector<World*>, compareItem >* q;
     public:
-        Grassland(int day, priority_queue<World*, vector<World*>, compareItem>* q1): World(day), q(q1){}
-        Rabbit* getRabbitClose(int x, int y);
+        Cells **cell;
+        Grassland(int day, priority_queue<World*, vector<World*>, compareItem >* q1): World(day), q(q1){
+        cell = new Cells*[512];
+        for(int i = 0; i < 512; i++)
+            cell[i] = new Cells[512];
+        }
+        void addWolf(int xWolf);
+        void addRabbit(int ar, int lr);
+        void addAnimals(int x, int y);
+       //Rabbit* getRabbitClose(int x, int y);
         //void grow(); 
-        void eatGrass(int x, int y, Rabbit*);
-        void Run();
-        ~Grassland();
+        //void eatGrass(int x, int y, Rabbit*);
+        virtual void Run(){cout << "In Grassland " << getDay() << endl;
+            setDay(getDay() + 25); q->push(this);}
+        ~Grassland(){delete cell;}
 };
 #endif
