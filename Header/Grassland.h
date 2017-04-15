0e2ef66a7028eb3c0ed33c1b76880a4c6e48d896
Gaebuidhe <faolannus@gmail.com>
2017-04-14 22:06:49 -0400

    Update Grassland.h

--- Header/Grassland.h
+++ Header/Grassland.h
@@ -5,6 +5,7 @@
 #include "Cells.h"
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 
 class Grassland : public World{
@@ -15,9 +16,30 @@ class Grassland : public World{
     //bool grass; 
     
     public:
-        Grassland(int day, priority_queue<World*, vector<World*>, compareItem>* q1): World(day), q(q1){}
+        Grassland(priority_queue<World*, vector<World*>, compareItem>* q1): World(day), q(q1){
+            *cell = new Cells[512][512];
+        };
         Rabbit* getRabbitClose(int x, int y);
         //void grow(); 
+        void createRabbit(int noL, int noA){
+            for(int i = 0; i < noL; i++){
+                //create noL active rabbits
+            }
+            for(int i = 0; i < noA; i++){
+                //create noA Lazy rabbits;
+            }
+        };
+        void createWolf(int no){
+            for(int i = 0; i < no; i++){
+                //create wolves
+            }
+        };
+        Animal* getAnimal(int x, int y){
+            return cell[x][y].a;
+        };
+        void moveinto(int x, int y, Animal& newA){
+            cell[y][z]->a = newA;
+        };
         void eatGrass(int x, int y, Rabbit*);
         void Run();
         ~Grassland();
