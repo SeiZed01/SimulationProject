0ceb2f93a6cf943aba6f4fdb0ce1826736d187e9
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-15 01:16:08 -0400

    Update Rabbit.h

--- Header/Rabbit.h
+++ Header/Rabbit.h
@@ -8,17 +8,29 @@ using namespace std;
 class ActiveRabbit : public Animal{
     public:
         ActiveRabbit(int k, bool hunger,  priority_queue<World*, vector<World*>, compareItem >* q1): Animal(k, hunger, q1){}
-        void* Eat(); // Still thinking
-        void Run(){cout << "This is an Active Rabbit: " << getDay() << endl;} //This will do something, gutta add more functionaly.. requeue, death etc..
+        //void* Eat(); // Still thinking
+        void Run(){
+            if(getDay() <= 10000){
+                cout << "This is an Active Rabbit: " << getDay() << endl;
+                setDay(getDay() + 2);
+                q->push(this);
+            }
+        } //This will do something, gutta add more functionaly.. requeue, death etc..
         ~ActiveRabbit(){}
 };
 
 class LazyRabbit : public Animal{
-    priority_queue<World*, vector<World*>, compareItem >* q;
     public:
         LazyRabbit(int k, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1): Animal(k, hunger, q1){}
         //void* Eat(); // Still thinking
-        void Run(){cout << "This is a Lazy Rabbit: " << getDay() << endl;} // This will do something still have add more functions like requeue and etc
+        void Run(){
+            if(getDay() <= 10000){
+                cout << "This is a Lazy Rabbit: " << getDay() << endl;
+                setDay(getDay() + 1);
+                q->push(this);
+            }
+        }
+        // This will do something still have add more functions like requeue and etc
         ~LazyRabbit(){}
 };
 
