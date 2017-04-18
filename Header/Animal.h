1b3e73878201269c1aaf68ba2033e08b1a7962f3
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-18 00:06:14 -0400

    Update Animal.h

--- Header/Animal.h
+++ Header/Animal.h
@@ -4,12 +4,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+class Grassland;
 class Animal : public World{
     bool hunger;
+    int ID;
     public:
+        int x, y;
+        Grassland* hold;
         priority_queue<World*, vector<World*>, compareItem >* q;
-        Animal(int day, bool hunger, priority_queue<World*, vector<World*>, compareItem>* q1): World(day), hunger(hunger), q(q1){}
+        Animal(int day, int i,  bool hunger, priority_queue<World*, vector<World*>, compareItem>* q1, Grassland* t): World(day), hunger(hunger), hold(t), q(q1), ID(i){}
+        int getID(){return ID;}
         void setHunger(bool k){hunger = k;}
         bool getHunger(){return hunger;}
         virtual void Run(){} // Implement death and hunger and etc in Run Func.
