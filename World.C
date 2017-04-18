22bc166ea476f15a9bcad3afaeca712f46388004
Gaebuidhe <faolannus@gmail.com>
2017-04-18 12:43:05 -0400

    Update World.C

--- World.C
+++ World.C
@@ -4,24 +4,24 @@
 
 using namespace std;
 
-World::World(){
+World::World(){ //the base class constructor
 	grass = true;
-	day = 0;
+	day = 0; //initial day start
 }
 
-World::~World(){
+World::~World(){ //basic destructor
 }
 
-void World::setGrasstrue(){
+void World::setGrasstrue(){ //sets grass boolean to true
 	grass = true;
 }
 
-void World::eatGrass(){
+void World::eatGrass(){ //sets grass boolean to false to represent it being eaten
 	grass = false;
-	growth = 9;
+	growth = 9; //represents the length of time needed to regrow
 }
 
-void World::grow(){
+void World::grow(){ //regrows grass
 	if(grass == false && growth > 0) growth--;
 	else if(grass == false && growth <= 0) setGrasstrue();
 }
