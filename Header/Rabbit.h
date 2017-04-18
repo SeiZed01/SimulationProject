8b2ea5c5d3e8cdf82ec27db0327c28bc67f115f5
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-18 01:01:27 -0400

    Update Rabbit.h

--- Header/Rabbit.h
+++ Header/Rabbit.h
@@ -10,9 +10,18 @@ class ActiveRabbit : public Animal{
         ActiveRabbit(int k,int i, bool hunger,  priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         //void* Eat(); // Still thinking
         void Run(){
-            if(getDay() <= 100){
-                cout << "This is an Active Rabbit: " << getDay() << endl;
+            if(getDay() <= 1000){
+                double probability = 0.05; // Probability of DEATH AFTER 750 Days
+                double result = rand() / RAND_MAX;
+                if(getDay() >= 750 && (result < probability)){
+                    hold->cell[this->x][this->y].a = NULL;
+                }
+                cout << "Day: " << getDay() << ", This is an Active Rabbit at: (" << x << "," << y << ")";
                 setDay(getDay() + 2);
+                setHunger(1);
+                if(getHunger() == 1){
+                    hold->eatGrass(x, y, this);
+                }
                 q->push(this);
             }
         } //This will do something, gutta add more functionaly.. requeue, death etc..
@@ -24,7 +33,10 @@ class LazyRabbit : public Animal{
         LazyRabbit(int k,int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         //void* Eat(); // Still thinking
         void Run(){
-            if(getDay() <= 100){
+            if(getDay() <= 10000){
+                if(getDay() == 450){ // death after 450 days
+                    hold->cell.a[this->x][this->y] = NULL;
+                }
                 cout <<"Day: " << getDay() << ", This is a Lazy Rabbit at : (" << x << "," << y << ")";
                 setDay(getDay() + 1);
                 setHunger(1);
@@ -33,7 +45,6 @@ class LazyRabbit : public Animal{
                 }
                 q->push(this);
             }
-            //cout << endl;
         }
         // This will do something still have add more functions like requeue and etc
         ~LazyRabbit(){}
