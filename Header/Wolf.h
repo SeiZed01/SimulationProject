d5bd73bef9ae3ad15662d41b9cdea6737a418abd
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-18 00:59:24 -0400

    Update Wolf.h

--- Header/Wolf.h
+++ Header/Wolf.h
@@ -6,11 +6,9 @@ using namespace std;
 class Wolf : public Animal{
     public:
         Wolf(int k, int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
-        //void doublePopulation();
-       // void* Eat(); // Might change Eat to Roam for all
         void Run(){
             if(getDay() <= 1000){
-                cout << "I am a Wolf : " << getDay() << endl;
+                cout << "Day: " << getDay() << ", I am a Wolf at: (" << x << "," << y << ")" << endl;
                 setDay(getDay() + 4);
                 q->push(this);
             }
