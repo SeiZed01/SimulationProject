e330a8465ca6fc2af05a5dfb714ac07c2977c590
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-18 00:04:50 -0400

    Update Rabbit.h

--- Header/Rabbit.h
+++ Header/Rabbit.h
@@ -2,15 +2,15 @@
 #define RABBIT_H
 #include <iostream>
 #include <vector>
+#include "Grassland.h"
 #include "Animal.h"
 using namespace std;
-
 class ActiveRabbit : public Animal{
     public:
-        ActiveRabbit(int k, bool hunger,  priority_queue<World*, vector<World*>, compareItem >* q1): Animal(k, hunger, q1){}
+        ActiveRabbit(int k,int i, bool hunger,  priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         //void* Eat(); // Still thinking
         void Run(){
-            if(getDay() <= 10000){
+            if(getDay() <= 100){
                 cout << "This is an Active Rabbit: " << getDay() << endl;
                 setDay(getDay() + 2);
                 q->push(this);
@@ -21,14 +21,19 @@ class ActiveRabbit : public Animal{
 
 class LazyRabbit : public Animal{
     public:
-        LazyRabbit(int k, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1): Animal(k, hunger, q1){}
+        LazyRabbit(int k,int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         //void* Eat(); // Still thinking
         void Run(){
-            if(getDay() <= 10000){
-                cout << "This is a Lazy Rabbit: " << getDay() << endl;
+            if(getDay() <= 100){
+                cout <<"Day: " << getDay() << ", This is a Lazy Rabbit at : (" << x << "," << y << ")";
                 setDay(getDay() + 1);
+                setHunger(1);
+                if(getHunger() == 1){
+                    hold->eatGrass(x, y, this);
+                }
                 q->push(this);
             }
+            //cout << endl;
         }
         // This will do something still have add more functions like requeue and etc
         ~LazyRabbit(){}
