9cac23068dfcd164a64fb9d75c058166de154759
Gaebuidhe <faolannus@gmail.com>
2017-04-18 13:42:20 -0400

    Update Wolf.h

--- Header/Wolf.h
+++ Header/Wolf.h
@@ -3,10 +3,10 @@
 #include "Animal.h"
 using namespace std;
 
-class Wolf : public Animal{
+class Wolf : public Animal{ //wolf inherits from Animal
     public:
         Wolf(int k, int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
-        void Run(){
+        void Run(){ //checks hunger
             if(getDay() <= 1000){
                 cout << "Day: " << getDay() << ", I am a Wolf at: (" << x << "," << y << ")" << endl;
                 setDay(getDay() + 4);
