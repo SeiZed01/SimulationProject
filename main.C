6a1fdd937d16936858635b500635932983b3ff21
Gaebuidhe <faolannus@gmail.com>
2017-04-18 12:37:17 -0400

    Update main.C

--- main.C
+++ main.C
@@ -9,21 +9,22 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]){
-    if(argc <= 2 || argc >=4)
-        cout << "Invalid number of arguments in the terminal! Please enter in two numbers separated by a space. First # is Wolf, Second is Rabbit" << endl;
-    else{
-        int wolf = atoi(argv[1]);
+int main(int argc, char* argv[]){ //main function takes in arguments for the number of wolves and rabbits
+    if(argc <= 2 || argc >=4) //error check message for improper variables.
+        cout << "Invalid number of arguments in the terminal!" << endl;
+        cout << "Please enter in two numbers separated by a space. First # is Wolf, Second is Rabbit" << endl;
+    else{ //creates wolves based on the input arguments and runs
+        int wolf = atoi(argv[1]); 
         int rabbits = atoi(argv[2]);
-        priority_queue<World*, vector<World*>, compareItem > pq;
-        Grassland* g1 = new Grassland(450, &pq);
-        g1->addAnimals(wolf, rabbits);
-        pq.push(g1);
-        while(!pq.empty()){
+        priority_queue<World*, vector<World*>, compareItem > pq; //creates priority queue
+        Grassland* g1 = new Grassland(450, &pq); //grassland created with constructor that takes queue
+        g1->addAnimals(wolf, rabbits); //creates the initial animals
+        pq.push(g1); //pushes g1 to the queue
+        while(!pq.empty()){ //runs the simulator
             World* i = pq.top();
             pq.pop();
-            i->Run();
+            i->Run(); //runs the world items, either Grassland, Wolf, Active Rabbit, or Lazy rabbit
         }
-        delete g1;
+        delete g1; //clean up
     }
 }
