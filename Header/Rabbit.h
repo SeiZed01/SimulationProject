5b8cb46b60ea9a2f454b0da48f2a75ad6561ac70
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 02:46:10 -0400

    Update Rabbit.h

--- Header/Rabbit.h
+++ Header/Rabbit.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "Grassland.h"
+#include <stdlib.h>
 #include "Animal.h"
 using namespace std;
 class ActiveRabbit : public Animal{ //Active rabbit inherits directly from animal
@@ -11,39 +12,15 @@ class ActiveRabbit : public Animal{ //Active rabbit inherits directly from anima
         void Run(){ //checks various conditions, hunger, death, etc
             if(getDay() <= 10000){
                 double probability = 0.05; // Probability of DEATH AFTER 750 Days
-                double result = rand() / RAND_MAX;
-                if(getDay() >= 750 && (result < probability)){ //checks chance of death after day 750
+                double result = (double)rand() / RAND_MAX;
+                if(getDay() >= 750 && (result <= probability)){ //checks chance of death after day 750
                     hold->cell[this->x][this->y].a = NULL;
+                    hold->rCount -= 1;
                     delete this; // gets rid of the animal
                 }
                 cout << "Day: " << getDay() << ", This is an Active Rabbit at: (" << x << "," << y << ")";
                 if(hold->cell[this->x][this->y].a != NULL){
                     setDay(getDay() + 2);
-                    setHunger(1);
-                    if(getHunger() == 1){ 
-                        hold->eatGrass(x, y, this);
-                    }
-                    q->push(this);
-                }
-                else
-                    cout << " This rabbit has died today.." << endl;
-            }
-        } 
-        ~ActiveRabbit(){}
-};
-
-class LazyRabbit : public Animal{
-    public:
-        LazyRabbit(int k,int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
-        void Run(){ //checks various conditions, hunger, death, etc
-            if(getDay() <= 10000){
-                if(getDay() == 450){ // death after 450 days
-                    hold->cell.a[this->x][this->y] = NULL;
-                    delete this;
-                }                
-                cout <<"Day: " << getDay() << ", This is a Lazy Rabbit at : (" << x << "," << y << ")";
-                if(hold->cell[this->x][this->y].a != NULL){
-                    setDay(getDay() + 1);
                     setHunger(1);
                     if(getHunger() == 1){
                         hold->eatGrass(x, y, this);
@@ -54,8 +31,33 @@ class LazyRabbit : public Animal{
                     cout << " This rabbit has died today.." << endl;
             }
         }
+        ~ActiveRabbit(){delete this;}
+};
 
-        ~LazyRabbit(){}
+class LazyRabbit : public Animal{
+    public:
+        LazyRabbit(int k,int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
+        void Run(){ //checks various conditions, hunger, death, etc            
+            if(getDay() <= 10000){
+                if(getDay() == 450){ // death after 450 days
+                    hold->cell[this->x][this->y].a = NULL;                    
+                    hold->rCount -= 1;
+                    delete this;
+                }
+                cout <<"Day: " << getDay() << ", This is a Lazy Rabbit at : (" << x << "," << y << ")";
+                if(hold->cell[this->x][this->y].a != NULL){                    
+                    setDay(getDay() + 1);
+                    setHunger(1);                    
+                    if(getHunger() == 1){
+                        hold->eatGrass(x, y, this);                    
+                    }
+                    q->push(this);
+                }                
+                else
+                    cout << " This rabbit has died today.." << endl;            
+            }
+        }
+        ~LazyRabbit(){delete this;}
 };
 
 #endif
