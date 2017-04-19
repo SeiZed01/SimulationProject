3654198135690972b5fc8b0701b82d61ac72d4e3
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 02:41:05 -0400

    Update World.h

--- Header/World.h
+++ Header/World.h
@@ -2,25 +2,32 @@
 //Copied & Modified from Dr. Tang's Item.h
 #ifndef WORLD_H_
 #define WORLD_H_
-
-class World{ //base class, stems to animal and grassland
+#include <functional>
+#include <vector>
+#include <queue>
+using namespace std;
+class World{
     int days;
     public:
-        World(); 
-        World(int day) : days(day){} 
+        World(){}
+        World(int day) : days(day){}
         friend class compareItem;
         int getDay(){return days;}
         void setDay(int day){days = day;}
-        virtual void Run();
+        virtual void Run(){}
         virtual ~World(){}
 
 };
 
-struct compareItem : binary_function<World*, World*, bool>{
-    bool operator(){const World* t1, const World* t2) const {
-        return (t1->day > t2->day);
+struct compareItem : public binary_function<World*, World*, bool>{
+    bool operator()(const World* t1, const World* t2) const {
+        return (t1->days > t2->days);
     }
 };
 
 
 #endif
+
+
+
+
