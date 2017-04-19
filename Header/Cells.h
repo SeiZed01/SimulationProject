8e6aee44a75577423eb23caf7c4918d11237d846
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 02:42:07 -0400

    Update Cells.h

--- Header/Cells.h
+++ Header/Cells.h
@@ -7,9 +7,9 @@ using namespace std;
 class Cells { //Cell class, counts as a basic tile point
 public:
     Animal* a; //points to animal residing in tile
-    int lastEaten; 
+    int lastEaten;
     int x, y;
     Cells(){lastEaten = 10; x = 0; y = 0; a = NULL;} // basic constructor sets initial values
-    ~Cells(){} //basic deconstructor
+    ~Cells(){delete this;} //basic deconstructor
 };
 #endif
