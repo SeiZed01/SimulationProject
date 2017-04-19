0c859d9ee2b05ccd07d52b7e8adfa18e56662fc6
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 19:46:52 -0400

    Update Wolf.h

--- Header/Wolf.h
+++ Header/Wolf.h
@@ -6,23 +6,30 @@ using namespace std;
 
 class Wolf : public Animal{ //wolf inherits from Animal
     public:
-        Wolf(int k, int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
+        Wolf(int k, int i, int hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         void Run(){ //checks hunger
             if(getDay() <= 10000){
+                cout << "Day: " << getDay() << ", I am a Wolf at: (" << x << "," << y << ")";
+
                 double probability = 0.01;
                 double result = (double)rand() / RAND_MAX;
                 if(getDay() >= 800 && (result < probability)){ // 1% chance to die after 800 days
-                    hold->cell[this->x][this->y].a = NULL;
                     hold->wCount -= 1;
-                    //delete this;
+                    hold->cell[this->x][this->y].a = NULL;
+//                    delete this;
                 }
-                cout << "Day: " << getDay() << ", I am a Wolf at: (" << x << "," << y << ")";
                 if(hold->cell[this->x][this->y].a != NULL){
                     setDay(getDay() + 4);
-                    q->push(this);
+                    if(getHunger() >= 0){
+                        hold->eatRabbit(x, y, this);
+                        hold->setHunger(0);
+                        q->push(this);
+                    }
+                   // q->push(this);
                 }
-                else
+                else{
                     cout << " this Wolf has died today.. " << endl;
+                }
             }
             cout << endl;
         }
