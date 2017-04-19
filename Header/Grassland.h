47fca7d4b08660e7a5f021330069a303acc51a3c
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 19:52:51 -0400

    Update Grassland.h

--- Header/Grassland.h
+++ Header/Grassland.h
@@ -13,26 +13,28 @@ class Grassland : public World{ //grassland class inherits from the world class
     public:
         int wCount = 0; // Wolf Counter
         int rCount = 0; // Rabbit Counter
+        //void roam(int x, int y, Animal*); // Wolf roams if cant find food.
         Cells **cell; //pointer to create two dimensional array
         Grassland(int day, priority_queue<World*, vector<World*>, compareItem >* q1): World(day), q(q1){
         cell = new Cells*[512]; //creates the tile area
         for(int i = 0; i < 512; i++)
             cell[i] = new Cells[512];
         }
-//        bool roam(int x, int y); // Checks surround area to see if place is available to eat or just roam around.
         void addWolf(int xWolf); // Helper Function to add animals
         void addRabbit(int ar, int lr); // Helper function to add animals
         void addAnimals(int x, int y); //Helper function to add animals
-       //Rabbit* getRabbitClose(int x, int y);
+        //void eatRabbit(int x, int y, Animal*);
         void eatGrass(int x, int y, Animal*);
-        void growGrass();
+        void growGrass(); // Grow grass each cell daily helper function
         virtual void Run(){
-            if(getDay() <= 1000){
+            if(getDay() <= 10000){
                 cout << "In Grassland " << getDay() << endl;
                 setDay(getDay() + 450);
                 q->push(this);
             }
         }
-        ~Grassland(){delete cell;} //cleans up cells
+        ~Grassland(){ // clean up cells
+            delete[] cell;
+        }
 };
 #endif
