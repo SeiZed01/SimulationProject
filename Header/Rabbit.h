b3c9da94fb089946f2a9441b1d4d523be239ab80
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 19:48:30 -0400

    Update Rabbit.h

--- Header/Rabbit.h
+++ Header/Rabbit.h
@@ -8,57 +8,62 @@
 using namespace std;
 class ActiveRabbit : public Animal{ //Active rabbit inherits directly from animal
     public:
-        ActiveRabbit(int k,int i, bool hunger,  priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
+        ActiveRabbit(int k,int i, int hunger,  priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         void Run(){ //checks various conditions, hunger, death, etc
             if(getDay() <= 10000){
+                cout <<"Day: " << getDay() << ", This is a Active Rabbit at : (" << x << "," << y << ")";
                 double probability = 0.05; // Probability of DEATH AFTER 750 Days
                 double result = (double)rand() / RAND_MAX;
                 if(getDay() >= 750 && (result <= probability)){ //checks chance of death after day 750
-                    hold->cell[this->x][this->y].a = NULL;
                     hold->rCount -= 1;
-                    //delete this; // gets rid of the animal
+                    hold->cell[this->x][this->y].a = NULL;
+//                    delete this; // gets rid of the animal
                 }
-                cout << "Day: " << getDay() << ", This is an Active Rabbit at: (" << x << "," << y << ")";
                 if(hold->cell[this->x][this->y].a != NULL){
                     setDay(getDay() + 2);
-                    setHunger(1);
-                    if(getHunger() == 1){
+                    if(getHunger() >= 0){
                         hold->eatGrass(x, y, this);
+                        setHunger(0);
                     }
                     q->push(this);
                 }
-                else
+                else{
                     cout << " This rabbit has died today.." << endl;
+                }
             }
+            cout << endl;
         }
-        ~ActiveRabbit(){delete this;}
+        ~ActiveRabbit(){}
 };
 
 class LazyRabbit : public Animal{
     public:
-        LazyRabbit(int k,int i, bool hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
-        void Run(){ //checks various conditions, hunger, death, etc            
+        LazyRabbit(int k,int i, int hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
+        void Run(){ //checks various conditions, hunger, death, etc
+            cout <<"Day: " << getDay() << ", This is a Lazy Rabbit at : (" << x << "," << y << ")";
             if(getDay() <= 10000){
                 hold->growGrass();
                 if(getDay() == 450){ // death after 450 days
-                    hold->cell[this->x][this->y].a = NULL;                    
                     hold->rCount -= 1;
-                   // delete this;
+                    hold->cell[this->x][this->y].a = NULL;
+                //    delete this;
                 }
-                cout <<"Day: " << getDay() << ", This is a Lazy Rabbit at : (" << x << "," << y << ")";
-                if(hold->cell[this->x][this->y].a != NULL){                    
+                if(hold->cell[this->x][this->y].a != NULL){
                     setDay(getDay() + 1);
-                    setHunger(1);                    
-                    if(getHunger() == 1){
-                        hold->eatGrass(x, y, this);                    
+                    if(getHunger() >= 0){
+                        hold->eatGrass(x, y, this);
+                        setHunger(0);
                     }
                     q->push(this);
-                }                
-                else
-                    cout << " This rabbit has died today.." << endl;            
+                }
+                else{
+                    cout << " This rabbit has died today.." << endl;
+                }
             }
+            cout << endl;
         }
-        ~LazyRabbit(){delete this;}
+
+        ~LazyRabbit(){}
 };
 
 #endif
