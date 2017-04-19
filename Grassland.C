3fd0572c2843f3b1deecf9d605075764f3b778bb
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 19:51:56 -0400

    Update Grassland.C

--- Grassland.C
+++ Grassland.C
@@ -14,7 +14,7 @@ void Grassland::addWolf(int xWolf){ //adds wolves based on number of argument xW
             x = rand() % 512;
             y = rand() % 512;
         }
-        cell[x][y].a = new Wolf(4, 4, false, q, this); //creates wolf at open coordinates
+        cell[x][y].a = new Wolf(4, 4, 0, q, this); //creates wolf at open coordinates
         wCount += 1;
         q->push(cell[x][y].a);
         //sets cell coordinates and stores them in cell and animal
@@ -35,15 +35,13 @@ void Grassland::addRabbit(int ar, int lr){ //takes in count of lazy and active r
             x = rand() % 512;
             y = rand() % 512;
         }
-        cell[x][y].a = new ActiveRabbit(2, 2,  true, q, this); //creates active rabbit
+        cell[x][y].a = new ActiveRabbit(2, 2,  0, q, this); //creates active rabbit
         rCount += 1;
         q->push(cell[x][y].a);
         //stores coordinates in cell and animal
         cell[x][y].x = x;
         cell[x][y].y = y;
-         cell[x][y].a->x = x;
-        cell[x][y].a->y = y;
-
+        cell[x][y].a->x = x;
     }
   //Adding Lazy Rabbits Below
     x = rand() % 512; //creates x coordinate
@@ -54,7 +52,7 @@ void Grassland::addRabbit(int ar, int lr){ //takes in count of lazy and active r
             x = rand() % 512;
             y = rand() % 512;
         }
-        cell[x][y].a = new LazyRabbit(1, 1, true, q, this);//creates lazy rabbit
+        cell[x][y].a = new LazyRabbit(1, 1, 0, q, this);//creates lazy rabbit
         rCount += 1;
         q->push(cell[x][y].a);
         //stores coordinates in cell and animal
@@ -73,17 +71,22 @@ void Grassland::addAnimals(int xWolf, int yRabbits){ //creates initial animals
     addRabbit(ar, lr);
 }
 
-/*bool Grassland::roam(int x, int y, Animal){
-      
+//Code Below is Roaming for Wolf, Logic should be correct. 
+/*void Grassland::roam(int x, int y, Animal* r){
+    bool found = false;
+    for(int i = max(0, x-16); i <= min(x+16, 511);i++){
+        if(found == 1)
+            break;
+        for(int j = max(0, y-16);j<=min(j+16,511)j++){
+            if(cell[i][j].a == NULL){
+                found = true;
+                cell[i][j].a = r;
+                cell[x][y].a = NULL; // after moves, set previous cell to NULL
+                break;
+            }
+        }
+    }
 }*/
-
-void Grassland::growGrass(){
-	for(int i = 0; i < 512; i++){
-		for(int j = 0; j < 512; j++){
-			if(cell[i][j].lastEaten < 10) cell[i][j].lastEaten++;
-		}
-	}
-}
 
 void Grassland::eatGrass(int x, int y, Animal* r){ //function takes in coordinates x,y and rabbit, eats grass at location
     if(r->getID() == 1){ // This is for Lazy Rabbit
@@ -91,17 +94,18 @@ void Grassland::eatGrass(int x, int y, Animal* r){ //function takes in coordinat
             if(r->getHunger() == 0) //if rabbit is not hungry, function ends
                 break;
             for(int j = max(0, y-5); j<= min(j+5, 511);j++){
-  if(i != x || j != y){ //checks if grass is available to be eaten
+                if(i != x || j != y){ //checks if grass is available to be eaten
                     if(cell[i][j].lastEaten == 10){
                         cell[i][j].a = r;
                         cell[i][j].lastEaten = 0;
-                        r->setHunger(0);
                         cell[i][j].a->x = i;
                         cell[i][j].a->y = j;
                          cout << ", Grass eaten at: (" << i << "," << j << ")" << endl;
                         cell[x][y].a = NULL; //removes rabbit from previous cell
                         break;
                         }
+                    else
+                        r->setHunger(getHunger() + 1);
                 }
             }
         }
@@ -115,15 +119,54 @@ void Grassland::eatGrass(int x, int y, Animal* r){ //function takes in coordinat
                     if(cell[i][j].lastEaten == 10){
                         cell[i][j].a = r;
                         cell[i][j].lastEaten = 0;
-                        r->setHunger(0);
                         cell[i][j].a->x = i;
                         cell[i][j].a->y = j;
                         cout << ", Grass eaten at : (" << i << "," << j << ")" << endl;
                         cell[x][y].a = NULL; //removes rabbit from previous cell
                         break;
                     }
+                    else
+                        r->setHunger(getHunger() + 1);
                 }
             }
+        }
+    }
+}
+
+//Below is the Code for eatingRabbits, please check logic.
+/*void Grassland::eatRabbit(int x, int y, Animal* r){ // Code to find Rabbit in a 65x65 Neighborhood.
+    bool test = false;
+    for(int i = max(0, x-32); i <= min(x+32, 511); i++){
+            if(test == 1){
+                break;
+            }
+        for(int j = max(0, y-32); j <= min(j+32, 511);j++){
+            if(i!= x || j!= y){ // checks boundaries
+                if(hold->cell[i][j].a->getID() == 2 || hold->cell[i][j].a->getID() == 1){
+                    test = true;
+                    cell[i][j].a = r;
+                    cell[i][j].a->x = i;
+                    cell[i][j].a->y = j;
+                    cout << ", Rabbit eaten at: (" << i << "," << j << ")" << endl;
+                    cell[x][y].a = NULL;
+                    break;
+                }
+                else
+                    r->setHunger(getHunger() + 1);
+            }
+        }
+    }
+    if(bool == false){ // If can't find food, roam with function from above.
+        roam(x, y, r);
+    }
+}*/
+
+// Helper function to inrease grass daily
+void Grassland::growGrass(){
+    for(int i = 0; i < 512; i++){
+        for(int j = 0; j < 512; j++){
+            if(cell[i][j].lastEaten < 10)
+                cell[i][j].lastEaten++;
         }
     }
 }
