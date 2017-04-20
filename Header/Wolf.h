590df2f3fd10044908592e3ed2da85cb261947af
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 22:32:18 -0400

    Update Wolf.h

--- Header/Wolf.h
+++ Header/Wolf.h
@@ -8,31 +8,35 @@ class Wolf : public Animal{ //wolf inherits from Animal
     public:
         Wolf(int k, int i, int hunger, priority_queue<World*, vector<World*>, compareItem >* q1, Grassland* t): Animal(k, i, hunger, q1, t){}
         void Run(){ //checks hunger
-            if(getDay() <= 10000){
+            if(getDay() <= 1000){
                 cout << "Day: " << getDay() << ", I am a Wolf at: (" << x << "," << y << ")";
 
                 double probability = 0.01;
                 double result = (double)rand() / RAND_MAX;
                 if(getDay() >= 800 && (result < probability)){ // 1% chance to die after 800 days
                     hold->wCount -= 1;
-                    hold->cell[this->x][this->y].a = NULL;
-//                    delete this;
-                }
-                if(hold->cell[this->x][this->y].a != NULL){
-                    setDay(getDay() + 4);
-                    if(getHunger() >= 0){
-                        hold->eatRabbit(x, y, this);
-                        setHunger(0);
-                        q->push(this);
-                    }
-                   // q->push(this);
-                }
-                else{
                     cout << " this Wolf has died today.. " << endl;
+                    hold->cell[this->x][this->y].a = NULL;
+                    delete this;
+                    return;
                 }
-            }
-            cout << endl;
-        }
-        ~Wolf(){delete this;}
-};
-#endif
+                if(getHunger() > 20){ // Death hasnt eaten in 20 days
+                    hold->cell[x][y].a = NULL;
+                    cout << " this Wolf has died today.. " << endl;
+                    hold->wCount -= 1;
+                    delete this;
+                    return;
+                }
+                if(hold->cell[x][y].a != NULL){
+                    setDay(getDay() + 4);
+                   /* if(getHunger() >= 0){ // Commented out line below check logic.
+                        Animal* r = hold->eatRabbit(x, y);
+                        if(r != NULL){
+                            hold->cell[r->x][r->y].a = this;
+                            this->x = r->x; this->y= r->y;
+                            setHunger(0);
+                            &r = NULL;
+                     
+                        }
+                        else{
+                            setHunger(getHunger() + 1);
