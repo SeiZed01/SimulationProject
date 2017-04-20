cc29a6d30a1ab14c792e09c80e594018fd4c0b02
Hung Nguyen <hungvnguyen0797@gmail.com>
2017-04-19 23:31:49 -0400

    Update Animal.h

--- Header/Animal.h
+++ Header/Animal.h
@@ -12,10 +12,10 @@ class Animal : public World{ //animal class inherits from world
         int x, y; //coordinates of current location
         Grassland* hold; //pointer towards the world
         priority_queue<World*, vector<World*>, compareItem >* q;
-        Animal(int day, int i,  bool hunger, priority_queue<World*, vector<World*>, compareItem>* q1, Grassland* t): World(day), hunger(hunger), hold(t), q(q1), ID(i){}
+        Animal(int day, int i,  int hunger, priority_queue<World*, vector<World*>, compareItem>* q1, Grassland* t): World(day), hunger(hunger), hold(t), q(q1), ID(i){}
         int getID(){return ID;} //returns ID
-        void setHunger(bool k){hunger = k;} //takes in boolean, sets hunger boolean
-        bool getHunger(){return hunger;} //returns hunger boolean
+        void setHunger(int k){hunger = k;} //takes in boolean, sets hunger boolean
+        int getHunger(){return hunger;} //returns hunger boolean
         virtual void Run(){} // Implement death and hunger and etc in Run Func.
         //virtual void* Eat(); // Find something to eat within certain parameter then delete pointer. 
        // virtual void doublePopulation();
